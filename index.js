06ab85d80bb343f42def5ef2b2a285057688ca0c
uzair <uazirfaiz@gmail.com>
2025-06-04 02:14:32 +0500

    update

---
+++ index.js
@@ -0,0 +1,52 @@
+// Forward Star Pattern
+// for (let i=1; i<=5; i++){
+// let pattern="";
+//     for (let j=1; j<=i; j++){
+//         pattern=pattern + '*';
+//     }
+//         console.log(pattern);
+
+// }
+
+
+// for (let i=6; i>=1; i--){
+// let pattern="";
+//     for (let j=1; j<=i; j++){
+//         pattern=pattern + '*';
+//     }
+//         console.log(pattern);
+
+// }
+// for (let i =1; i<=5; i++){
+//   var uzair="";
+//   for(let j=1 ; j<=i; j++)
+//   uzair = uzair + "1"
+//   console.log(uzair);
+
+// }
+
+// Numbering Pattern
+let rows=4;
+
+
+
+
+for (let i = 1; i <= rows; i++) {
+  let pattern = "";
+  for (let j = 1; j <= i; j++) {
+    pattern+= j;
+  }
+  console.log(pattern);
+}
+
+
+// // Reverse Numbering Pattern
+// let rows=4;
+
+// for(let i=1; i<=rows; i++){
+//   let pattern="";
+//   for(let j=1; j<=i; j++){
+//     pattern+=  '*';
+//   }
+//   console.log(pattern);
+// }
